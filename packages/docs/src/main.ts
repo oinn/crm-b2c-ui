b018e72cf11eed05254c9ba491da831f89c77426
Vladimir Zaguliaev <oinnwork@outlook.com>
2022-04-12 10:52:04 +0300

    docs: Added global styles

--- packages/docs/src/main.ts
+++ packages/docs/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import UiKit from '@crm-b2c-ui/kit';
 import App from './App.vue'
 import router from './routes';
+import './styles/index.scss';
 import '@crm-b2c-ui/kit/dist/style.css';
 
 createApp(App)
